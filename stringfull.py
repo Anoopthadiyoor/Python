4c79204c68d058be9bc89a52365ab380a833bdd9
Anoop krishnan <anoopthadiyoor123@gmail.com>
2025-04-29 00:11:38 +0530

    Add files via upload

---
+++ stringfull.py
@@ -0,0 +1,16 @@
+mess='hello world'
+m= mess.split()
+print(m)
+#string reversing
+print(mess[::-1])
+ab=mess.split()
+print(mess.count('l'))
+anoop=mess.replace('world','universe')
+print(anoop)
+print(mess.find('w'))
+print(mess[1:4])
+print(mess[-1::])
+hello= f'{mess} NA { anoop}'
+print(hello)
+aaa='anoop is a good boy'
+
